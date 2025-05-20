ad8ee5f68f6a48a0392c200c3deacdff5cba1c17
YoussefMennani <youssefmennani1996@gmail.com>
2025-05-20 22:42:26 +0100

    20/05/2025

--- src/main/java/org/example/ClientHandler.java
+++ src/main/java/org/example/ClientHandler.java
@@ -1,16 +1,39 @@
 package org.example;
-import java.io.*;
-import java.net.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Date;
+
+import com.fleetManagementSystem.commons.position.dto.PositionResponse;
+import com.fleetManagementSystem.commons.position.model.Position;
 import com.google.gson.Gson;
-import java.io.*;
-import java.net.*;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.bson.Document;
+import org.bson.types.ObjectId;
 
 public class ClientHandler implements Runnable {
 
     private final Socket clientSocket;
+    private String imei;
 
-    public ClientHandler(Socket socket) {
+    private ObjectId vehicle;
+    KafkaProducer<String, String> producer;
+
+    MongoCollection<Document> positionsCollection;
+    MongoCollection<Document> vehiclesCollection;
+    public ClientHandler(Socket socket, KafkaProducer<String, String> producer, MongoCollection<Document> positionsCollection,MongoCollection<Document> vehiclesCollection) {
         this.clientSocket = socket;
+        this.imei = null;
+        this.producer = producer;
+        this.positionsCollection = positionsCollection;
+        this.vehiclesCollection = vehiclesCollection;
     }
 
     @Override
@@ -21,7 +44,29 @@ public class ClientHandler implements Runnable {
             // Read IMEI from the client, only once after connection
             String imei = in.readLine();
             if (imei != null) {
+                this.imei = imei;
                 System.out.println("Received IMEI: " + imei);  // Process IMEI
+
+                Document query = new Document("tracker.imei", imei);
+                Document result = vehiclesCollection.find(query).first();
+//                if (result != null) {
+//                    System.out.println("Found vehicle: " + result.toJson());
+//                    System.out.println("id Found vehicle: " + result.toJson());
+//                } else {
+//                    System.out.println("No vehicle found with the given IMEI.");
+//                }
+
+                if (result != null) {
+                    // Extract _id
+                    this.vehicle = (ObjectId) result.get("_id");
+                    System.out.println("Vehicle ID: " + vehicle);
+                } else {
+                    System.out.println("No vehicle found with the given IMEI.");
+                }
+
+            } else {
+                System.out.println("IMEI not received; client might have disconnected.");
+                return;  // Exit if IMEI is not received
             }
 
             // Instantiate Gson for JSON parsing
@@ -30,15 +75,69 @@ public class ClientHandler implements Runnable {
             // Now handle the position data continuously
             String inputLine;
             while ((inputLine = in.readLine()) != null) {
-                // Parse the received JSON string into GPSPosition object
-                GPSPosition position = gson.fromJson(inputLine, GPSPosition.class);
+                try {
+                    // Parse the received JSON string into GPSPosition object
+                    Position position = gson.fromJson(inputLine, Position.class);
+                    //update last position
+//                    Position positionData = Position.builder()
+//                            .latitude(position.getLatitude())
+//                            .longitude(position.getLongitude())
+//                            .vehicleID(vehicle.toString())
+//                            .imei(position.getImei())
+//                            .speed(position.getSpeed())
+//                            .timestamp(position.getTimestamp())
+//                            .altitude(position.getAltitude())
+//                            .heading(position.getHeading())
+//                            .metrics(position.getMetrics())
+//                            .build();
+                    position.setVehicleID(vehicle.toString());
+                    // Print the GPS position object
+                    System.out.println("Received GPS Position from IMEI "+this.imei+" : " + position);
 
-                // Print the GPS position object
-                System.out.println("Received GPS Position: " + position);
+                    // Convert GPSPosition object to a BSON Document
+                    Document document = Document.parse(gson.toJson(position));
 
-                // Send an acknowledgment back to the client
-                out.println("ACK: Position received");
-                out.flush();  // Ensure the acknowledgment is sent immediately
+                    Date now = new Date(); // Current date and time
+                    document.put("createdAt", now);
+                    document.put("updatedAt", now);
+
+                    // Insert the document into MongoDB
+                    positionsCollection.insertOne(document);
+
+                    System.out.println("Document inserted successfully!");
+
+                    System.out.println(" =============== < send Position to KAFKA TOPIC > ================= ");
+
+                    // create a producer record
+                    ProducerRecord<String, String> producerRecord =
+                            new ProducerRecord<>("position-updates", inputLine);
+
+                    producer.send(producerRecord);
+                    producer.flush();
+
+                    //update last position
+                    PositionResponse lastPosition = PositionResponse.builder()
+                            .latitude(position.getLatitude())
+                            .longitude(position.getLongitude())
+                            .imei(position.getImei())
+                            .speed(position.getSpeed())
+                            .timestamp(position.getTimestamp())
+                            .altitude(position.getAltitude())
+                            .heading(position.getHeading())
+                            .metrics(position.getMetrics())
+                            .build();
+
+
+                    Document documentLastPosition = Document.parse(gson.toJson(lastPosition));
+                    Document update = new Document("$set", new Document("lastPosition", documentLastPosition));
+                    vehiclesCollection.updateOne(new Document("_id", this.vehicle), update);
+
+
+                    // Send an acknowledgment back to the client
+                    out.println("ACK: Position received");
+                } catch (Exception e) {
+                    System.err.println("Failed to parse GPS data: " + e.getMessage());
+                }
             }
 
         } catch (IOException e) {
@@ -47,6 +146,7 @@ public class ClientHandler implements Runnable {
             try {
                 clientSocket.close(); // Close the socket when done
                 System.out.println("Client disconnected.");
+                producer.close();
             } catch (IOException e) {
                 System.err.println("Error closing client socket: " + e.getMessage());
             }
--- src/main/java/org/example/GPSListener.java
+++ src/main/java/org/example/GPSListener.java
@@ -1,7 +1,17 @@
 package org.example;
 
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.bson.Document;
+
 import java.io.*;
 import java.net.*;
+import java.util.Properties;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -13,15 +23,39 @@ public class GPSListener {
     public static void main(String[] args) {
         ExecutorService executor = Executors.newFixedThreadPool(MAX_THREADS); // Thread pool for handling clients
 
-        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+
+
+        String bootstrapServers = "127.0.0.1:9092";
+
+        // create Producer properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // create the producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+
+        try (ServerSocket serverSocket = new ServerSocket(PORT); MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017")) {
             System.out.println("GPS Listener started on port " + PORT);
+
+            // Access the database
+            MongoDatabase geoPositionDB = mongoClient.getDatabase("GeoPositionDB");
+            MongoDatabase geoTrackDB = mongoClient.getDatabase("GeoTrackDB");
+
+
+            // Access the positionsCollection
+            MongoCollection<Document> positionsCollection = geoPositionDB.getCollection("positions");
+            MongoCollection<Document> vehiclesCollection = geoTrackDB.getCollection("vehicles");
+
 
             while (true) {
                 Socket clientSocket = serverSocket.accept(); // Accept incoming client connection
                 System.out.println("New client connected: " + clientSocket.getInetAddress());
 
                 // Assign each client to a new thread
-                executor.submit(new ClientHandler(clientSocket));
+                executor.submit(new ClientHandler(clientSocket,producer,positionsCollection,vehiclesCollection));
             }
 
         } catch (IOException e) {
